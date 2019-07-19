14e507464b30790e0aed258ad9d9889ae059e1b5
deemoding <wsd009811@gmail.com>
2019-07-19 19:05:38 +0800

    feat: 加页面基本结构

---
+++ README.md
@@ -0,0 +1,8 @@
+kyoto默哀页面
+==============
+
+https://reactcommunity.org/react-transition-group/css-transition
+
+https://reacttraining.com/react-router/web/example/animated-transitions
+
+https://www.zhangxinxu.com/wordpress/2019/05/css-transfer-animation/
--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,36 @@
 import React from 'react';
+import { TransitionGroup, CSSTransition } from "react-transition-group";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+import style from "./App.less";
 
 class App extends React.PureComponent {
   render() {
-    return <div>hello</div>;
+    return (
+      <Router>
+        <Route
+          render={({ location }) => (
+            <div>
+              <TransitionGroup>
+                <CSSTransition
+                  key={location.key}
+                  className={style.page}
+                >
+                  <Switch location={location}>
+                    <Route exact path="/" />
+                    <Route path="/page1" />
+                    <Route path="/page2" />
+                  </Switch>
+                </CSSTransition>
+              </TransitionGroup>
+            </div>
+          )}
+        />
+      </Router>
+    );
   }
 }
 

b899944d4ffe226c80ee6b8148bf52f77d747e4b
DeemoDing <wsd009811@gmail.com>
2019-07-28 19:17:05 +0800

    feat: change css for pages; add auto scroll to page2

--- src/Pages/Page2/index.jsx
+++ src/Pages/Page2/index.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { withRouter } from "react-router";
+import getScrollBottom from "../../utils/getScrollBottomHeight";
 import letters from "./letter";
 import style from "./style.less";
 
@@ -10,6 +11,8 @@ class Page2 extends React.Component {
   constructor(props) {
     super(props);
     this.timer = 0;
+    this.timerScroll = 0;
+    this.scrollTop = 0;
     this.state = {
       len: 0,
     };
@@ -22,7 +25,16 @@ class Page2 extends React.Component {
   componentDidMount() {
     this.timer = setInterval(() => {
       this.showNext();
-    }, Math.round(3000 / LETTERS_LEN));
+    }, Math.round(180000 / LETTERS_LEN));
+    const scrollBottom = getScrollBottom();
+    this.timerScroll = setInterval(() => {
+      if (this.scrollTop >= scrollBottom) {
+        clearInterval(this.timerScroll);
+      } else {
+        this.scrollTop += scrollBottom * 16 / 180000;
+        window.scrollTo(0, this.scrollTop);
+      }
+    }, 16);
   }
 
   showNext() {
@@ -44,7 +56,7 @@ class Page2 extends React.Component {
     let i = 0;
     while (len > 0) {
       content.push(
-        <p key={i} className={i === letters.length - 1 && style.luokuan}>
+        <p key={i} className={i === letters.length - 1 ? style.luokuan : ''}>
           {letters[i].slice(0, len)}
         </p>
       );

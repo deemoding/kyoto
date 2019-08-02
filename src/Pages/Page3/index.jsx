cb14b6c044a9fac54011e1065a22e7a2e7d9ca01
deemoding <wsd009811@gmail.com>
2019-08-02 18:55:54 +0800

    feat(Page3): add http request

--- src/Pages/Page3/index.jsx
+++ src/Pages/Page3/index.jsx
@@ -1,21 +1,58 @@
 import React from "react";
+import axios from "axios";
+import config from "../../config";
 import style from "./style.less";
 import flowerImg from "./img/flower.png";
 import bgImg from "./img/bg.png";
 import bgPurpleImg from "./img/bgPurple.png";
 
+const myAxios = axios.create({
+  baseURL: config.ajaxURL,
+  responseType: 'json',
+});
+
 class Page3 extends React.Component {
   constructor(props) {
     super(props);
+    this.getting = false;
+    this.posting = false;
     this.state = {
       sent: false,
+      nums: 0,
     };
   }
 
+  componentDidMount() {
+    this.getting = true;
+    myAxios.get('/api/flower/')
+      .then(rsp => {
+        if (rsp.data.status === 200) {
+          this.setState({
+            nums: rsp.data.num
+          });
+        }
+      })
+      .finally(() => {
+        this.getting = false;
+      });
+  }
+
   onClick() {
-    this.setState({
-      sent: true,
-    });
+    if (!(this.getting || this.posting)) {
+      this.posting = true;
+      myAxios.post('/api/flower/')
+        .then(rsp => {
+          if (rsp.data.status === 200) {
+            this.setState({
+              nums: rsp.data.num,
+              sent: true,
+            });
+          }
+        })
+        .finally(() => {
+          this.posting = false;
+        });
+    }
   }
 
   render() {
@@ -27,7 +64,11 @@ class Page3 extends React.Component {
         }}
       >
         <div className={style.text}>
-          <p>作死小王子通过这里对这次事件表达了自己的祝福并献上了花束</p>
+          <p>
+            已有
+            {this.state.nums > 0 ? this.state.nums : '多'}
+            人通过这里对这次事件表达了自己的祝福并献上了花束
+          </p>
           <p>点击献花</p>
           <img
             className={style.flower}
